793a80005beef7a50be89051e0a18488d0daddfa
Lucas Vieira dos Santos <lucas6eng@gmail.com>
2026-01-18 09:48:52 +0100

    add config

--- .config/nvim/lua/lucas/plugins/copilot.lua
+++ .config/nvim/lua/lucas/plugins/copilot.lua
@@ -1,5 +1,14 @@
 return {
 	{
 		"github/copilot.vim",
+		event = "InsertEnter",
+		config = function()
+			-- Request suggestion explicitly - YOUR MAIN REQUEST
+			vim.keymap.set("i", "<M-s>", "<Plug>(copilot-suggest)", { silent = true })
+		end,
+		keys = {
+			{ "<leader>cp", "<cmd>Copilot panel<cr>", desc = "Copilot Panel" },
+			{ "<leader>ct", "<cmd>Copilot status<cr>", desc = "Copilot Status" },
+		},
 	},
 }
